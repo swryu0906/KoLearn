04274ecae4b9c65d200b3ffdb94a7b9c3ccd0e35
Walli Smith <walli.smith04@gmail.com>
2015-07-17 15:24:06 -0400

    style login page

--- db/schema.rb
+++ db/schema.rb
@@ -22,22 +22,19 @@ ActiveRecord::Schema.define(version: 20150717094211) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "name"
+    t.string   "category"
     t.string   "city_name"
-    t.string   "image"
     t.text     "description"
     t.integer  "user_id"
-    t.integer  "category_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
 
-  add_index "courses", ["category_id"], name: "index_courses_on_category_id"
   add_index "courses", ["user_id"], name: "index_courses_on_user_id"
 
   create_table "events", force: :cascade do |t|
     t.datetime "start_time"
     t.datetime "end_time"
-    t.integer  "counter"
     t.integer  "course_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
